6fbe9d293401aa3360bc5a1234a6c3b123cbc20a
sujonphero <sujonsheikh.in@gmail.com>
2024-08-15 20:46:06 +0600

    fetch movies data from tmdb

---
+++ components/Movie.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const Movie = ({ movie }) => {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-slate-700 font-semibold line-clamp-1">
+        {movie.title}
+      </h1>
+      <Image
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+        width={400}
+        height={400}
+        alt={movie.title}
+        className="rounded-md"
+      />
+    </div>
+  );
+};
+
+export default Movie;
